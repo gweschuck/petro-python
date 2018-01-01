b1dd615a0e81aae2de6f9a8ea5e77f14aab9ebd2
Wesley C. Williams <wcwmit@gmail.com>
2018-01-01 15:38:17 -0600

    Add header

---
+++ blackoil.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""blackoil.py: A black oil calculator from William D. McCain, Jr."""
+
+__author__ = "Wesley C. Williams"
+__copyright__ = "Copyright 2007, Wesley C. Williams"
+__credits__ = ["Wesley C. Williams"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Wesley C. Williams"
+__email__ = "wcwmit@gmail.com"
+__status__ = "Production"
+
+GOR = input("Input Gas Oil Ratio in SCF/STB: ")
+print GOR
+1
